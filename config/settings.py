6a14b5515d557b2545cb10bf527a5b834c6f47bf
Francois <Fpaul1274@gmail.com>
2022-10-17 14:59:52 +0200

    fixed error with allowed hosts

--- config/settings.py
+++ config/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["https://web-production-4c1b.up.railway.app/"]
+ALLOWED_HOSTS = ["web-production-4c1b.up.railway.app/"]
 
 
 # Application definition
