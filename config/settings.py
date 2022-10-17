4fb244394c4ad2721bd1ad034168da3b5565b9d9
Francois <Fpaul1274@gmail.com>
2022-10-17 15:17:58 +0200

    updated DEBUG for live site to False

--- config/settings.py
+++ config/settings.py
@@ -28,7 +28,7 @@ env.read_env()
 SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env("DEBUG", True)
 
 ALLOWED_HOSTS = ["todo-api.up.railway.app", "localhost"]
 
